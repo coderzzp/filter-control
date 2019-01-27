b9ae5f54f06b3eb5dbc34328a9ba9ace1bf1d189
zzp <zzp@zzpdeMBP.lan>
2019-01-27 15:12:50 +0800

    add error test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = str =>{
   if(typeof str !=='string'){
-    throw Error('请传入一个字符串')
+    throw new Error('请传入一个字符串')
   }else{
     // 控制字符的ASSCII码：
     const controlCodeList = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,127]
--- test/index.test.js
+++ test/index.test.js
@@ -3,3 +3,10 @@ const filterControl = require('../src/index.js');
 test('filter \\n', () => {
   expect(filterControl('abc\ndef')).toBe('abcdef');
 });
+
+test('expect string', () => {
+  function testError(){
+    filterControl(123)
+  }
+  expect(testError).toThrowError();
+});
